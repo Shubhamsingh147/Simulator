c43cd292efd4d817ec81fa9439819205b60f29d6
shubhamsingh147 <shubhamsingh147@gmail.com>
2014-09-08 18:48:38 +0000

    interface.h edited to remove switch-case

--- interface.h
+++ interface.h
@@ -6,47 +6,41 @@
 #include <time.h>
 
 using namespace std;
-
+void run_prog(string c)
+{
+	system(c.c_str());	
+	system("./temp.out  "); 
+}
 int main(int argc, char *argv[]){
 	
-	int i=0;
+	int i=0;string c;
 	string file[100];
 	float arrival_time[100],burst_time[100];
+	int priority[100];
 	clock_t start_time,end_time;
 	system("clear");
-	cout<<"Press 0 for file name to stop entering files!"<<endl;
+	cout<<"Ensure that files are in the same folder(sample files are p1.c & p2.c) \nPress 0 in file name to stop entering files!"<<endl;
 	while(1){
-		
-		
-		cout<<"Enter file name "<<i+1<<": ";
-		cin>>file[i];
+		cout<<"Enter name of file #"<<i+1<<": ";	cin>>file[i];
 
 		if( file[i][0]=='0')
 			break;
 		
-		cout<<"Enter it's arrival time: ";
-		cin>>arrival_time[i];
-		cout<<endl;
+		cout<<"Enter it's arrival time: ";		cin>>arrival_time[i];
+		cout<<"Enter it's Priority(unique int): ";		cin>>priority[i];
 		//Execute the given program according to it's type(C++/C)
-
-		string c;
-
 		if(file[i][sizeof(file[i])]=='c')
 			c="gcc -o temp.out -pthread "+file[i];
 		else
 			c="g++ -o temp.out -pthread "+file[i]; 
-		
 		start_time = clock();
-    	//cout<<"Process starting at "<<start_time<<endl;
-    	
-    	system(c.c_str());
-    	system("./temp.out");
+		  			//compile program to temp.out
+    	run_prog(c); //run the temp.out file
 		end_time = clock();
-
-		burst_time[i] = (((float)end_time - (float)start_time) / 1000000.0F ) * 1000;
-		//cout<<"Process ended at "<<end_time<<endl;
-		
-		cout<<"Calculating Burst time: "<<burst_time[i]<<endl;
+		system("rm temp.out");
+		cout<<"Calculating Burst time: ";
+		burst_time[i] = (((float)end_time - (float)start_time) / 1000000.0F ) * 1000; //4900/1000
+		cout<<burst_time[i]<<endl;
 		cout<<"******************************************************"<<endl;
 		i++;
 	}
@@ -54,14 +48,13 @@ int main(int argc, char *argv[]){
 		
 		//Write to file
 		std::ofstream outfile;
-		system("rm process.txt");
-		outfile.open("process.txt", std::ios_base::app );
-		outfile << "#file_name" << "\t" << "arrival_time"<<"\t"<<"burst_time"<< endl;
+		outfile.open("process.txt");
+		outfile << "#file_name\tarrival_time\tburst_time\tpriority"<<endl;
 		for(int j=0;j<i;j++)
-		outfile << file[j] << "\t" << arrival_time[j]<<"\t"<<burst_time[j]<<endl;
+		outfile << file[j] << "\t" << arrival_time[j]<<"\t"<<burst_time[j]<<"\t"<<priority[j]<<endl;
 		//Writing Ends
 
-		int t;
+		/*int t;
 		cout<<"Which algorithm do you want to implement among these processes? (1).SJF (2).FCFS (3).SRTF :";
 		cin>>t;
 		while(t!=1 && t!=2 && t!=3){
@@ -83,7 +76,7 @@ int main(int argc, char *argv[]){
 			  break;
 			default:
 			  break;
-		}
+		}*/
 	}
 	else cout<<"No files"<<endl;
 }
